06312ea825f8c16d7438027c3d939d57de585fc2
user131656 <user131656@ny218.local>
2018-01-06 16:06:39 -0500

    cleaning DetailViewController

--- myCrypto/DetailViewController.swift
+++ myCrypto/DetailViewController.swift
@@ -1,5 +1,3 @@
-
-
 import UIKit
 
 class DetailViewController: UIViewController {
@@ -18,14 +16,10 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         configureView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     var detailItem: NSDate? {
         didSet {
