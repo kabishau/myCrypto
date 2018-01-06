0e112ab430d2cab2be2480a16cc38806e64b30f2
user131656 <user131656@ny218.local>
2018-01-06 15:45:27 -0500

    extension for view controller  has been created and methods for table view controllers have been moved there

--- myCrypto/source/Modules/CryptoList/CryptoListViewController.swift
+++ myCrypto/source/Modules/CryptoList/CryptoListViewController.swift
@@ -46,38 +46,22 @@ class CryptoListViewController: UITableViewController {
         }
     }
 
-    // MARK: - Table View
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
+// MARK: - Table View
+extension CryptoListViewController {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
     }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
 
 }
 
