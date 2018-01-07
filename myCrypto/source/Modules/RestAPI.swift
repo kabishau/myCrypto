3a84842d02c67ed52e8274c35fd8f427ce9ddf14
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-01-06 20:46:31 -0500

    class that gets data from API and saves it as array of arrays [Currency]

--- myCrypto/source/Modules/RestAPI.swift
+++ myCrypto/source/Modules/RestAPI.swift
@@ -1,1 +1,34 @@
 import Foundation
+
+class RestAPI {
+    
+    func coinsList(complition: @escaping (([Currency]?) -> Void)) {
+        guard let url = URL(string: "https://min-api.cryptocompare.com/data/all/coinlist") else {
+            complition(nil)
+            print("url is invalid")
+            return
+        }
+        
+        let request = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let unwrappedData = data, error == nil else {
+                complition(nil)
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: unwrappedData, options: .allowFragments) as? [String: Any],
+                    let content = json["Data"] as? [String: Any] else {
+                        complition(nil)
+                        return
+                }
+                let currencies = content.flatMap(Currency.init)
+                complition(currencies)
+            } catch {
+                complition(nil)
+                print(error.localizedDescription)
+            }
+        }
+        request.resume()
+    }
+}
